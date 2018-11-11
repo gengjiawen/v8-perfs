6bd5b96da85555ac3a10d10102eab8572780e12b
gengjiawen <technicalcute@gmail.com>
2018-11-11 18:28:46 +0800

    prettify

--- primes.js
+++ primes.js
@@ -3,12 +3,12 @@ function Primes() {
   this.primes = new Array(25000)
   this.getPrimeCount = () => this.prime_count
   this.getPrime = i => this.primes[i]
-  this.addPrime = i => this.primes[this.prime_count++] = i
+  this.addPrime = i => (this.primes[this.prime_count++] = i)
   this.isPrimeDivisible = candidate => {
-    for(let i = 1; i < this.prime_count; ++i) {
-       if (candidate % this.primes[i] == 0) {
-         return true
-       }
+    for (let i = 1; i < this.prime_count; ++i) {
+      if (candidate % this.primes[i] == 0) {
+        return true
+      }
     }
     return false
   }
